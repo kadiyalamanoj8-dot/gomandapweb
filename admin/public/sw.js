2f0e1967b3a81ece6f934564da5fd741700ab6b8
kadiyalamanoj8-dot <kadiyalamanoj8@gmail.com>
2026-06-16 08:56:18 +0530

    Add killer sw.js

--- admin/public/sw.js
+++ admin/public/sw.js
@@ -4,10 +4,25 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    self.clients.claim().then(() => {
-      self.registration.unregister().then(() => {
-        console.log('Orphaned Service Worker successfully killed.');
-      });
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
     })
   );
+});
+
+// Forcefully unregister itself
+self.addEventListener('fetch', (e) => {
+  // Do nothing, just pass through
+});
+
+self.registration.unregister().then(function() {
+  return self.clients.matchAll();
+}).then(function(clients) {
+  clients.forEach(client => client.navigate(client.url))
 });
