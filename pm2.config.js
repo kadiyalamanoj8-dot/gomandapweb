57b5c1b1cc04e0f291108d8337a91de966a5d166
kadiyalamanoj8-dot <kadiyalamanoj8@gmail.com>
2026-06-16 21:09:34 +0530

    perf: switch frontend from PM2 to Nginx direct serving

--- pm2.config.js
+++ pm2.config.js
@@ -16,39 +16,6 @@ module.exports = {
         AWS_SUPPRESS_NODE_VERSION_WARNING: '1',
         NODE_NO_WARNINGS: '1'
       }
-    },
-    {
-      name: 'gomandap-client',
-      script: 'serve',
-      cwd: HOME,
-      env: {
-        PM2_SERVE_PATH: `${HOME}/client/dist`,
-        PM2_SERVE_PORT: 3000,
-        PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: '/index.html'
-      }
-    },
-    {
-      name: 'gomandap-vendor',
-      script: 'serve',
-      cwd: HOME,
-      env: {
-        PM2_SERVE_PATH: `${HOME}/vendor/dist`,
-        PM2_SERVE_PORT: 3001,
-        PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: '/index.html'
-      }
-    },
-    {
-      name: 'gomandap-admin',
-      script: 'serve',
-      cwd: HOME,
-      env: {
-        PM2_SERVE_PATH: `${HOME}/admin/dist`,
-        PM2_SERVE_PORT: 3002,
-        PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: '/index.html'
-      }
     }
   ]
 };
